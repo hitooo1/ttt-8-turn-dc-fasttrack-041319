535702c55365952bb6a3d293e89786d6e879f2fa
Bob N <hitooo@gmail.com>
2019-04-14 15:10:12 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,7 +14,10 @@ def move(array, index, value = "X")
   array[index] = value 
 end
 
-def valid_move()
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
 
 def turn(board)
   puts "Please enter 1-9:"
